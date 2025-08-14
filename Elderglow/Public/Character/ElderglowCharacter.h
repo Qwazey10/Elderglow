968cd840353665a0bb8320579cf48d69c046bcc5
MikePC <michaelgagno@gmail.com>
2025-08-13 18:58:11 -0700

    Init Commit -- Elderglow Experience Tables

--- Elderglow/Public/Character/ElderglowCharacter.h
+++ Elderglow/Public/Character/ElderglowCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Character/LivingBeingCharacterBase.h"
+#include "DataAssets/RaceIDMeshRefDataAsset.h"
 #include "ElderglowCharacter.generated.h"
 
 /**
@@ -14,7 +15,32 @@ class ELDERGLOW_API AElderglowCharacter : public ALivingBeingCharacterBase
 {
 	GENERATED_BODY()
 	
+	public:
+
+	/*
+	 * RaceID, this will govern which race we assign from an array of naked base mesh references. 
+	 * RaceID References:
+	 *
+	 * This data will come from the PlayerState
+	 *
+	 * Human = 0;
+	 * Woodelf = 1;
+	 * Ogre = 3
+	 */
+
+	//RaceID Base Mesh References, this holds the references of base meshes used per race. 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="RaceID Data Asset", meta=(AllowPrivateAccess=true))
+	URaceIDMeshRefDataAsset* RaceIDMeshRefData;
+
+	//Function to switch on race and assign the respective DataAsset. RaceID is stored in the PlayerState.
+	void AssignMeshDataAsset();
 	
-	
+	//Skeletal Mesh Component to act as the Primary Pose Component Target, this is the Chest mesh.  
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Character Parent Mesh - Chest", meta=(AllowPrivateAccess=true))
+	USkeletalMeshComponent* CharacterNPCDisplayMesh;
+
+	USkeletalMeshComponent* GetElderglowCharacterNPCDisplayMesh() const {return CharacterNPCDisplayMesh;}
+
+
 	
 };
